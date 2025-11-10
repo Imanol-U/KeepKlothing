a73501823bf7eba09f9b0b40ab7eecccf474da10
bluedragon011 <adrian.baz@opendeusto.es>
2025-11-10 18:22:47 +0100

    Prueba

--- keepKlothing/settings.py
+++ keepKlothing/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-&49yp&b4ng&@zvk7ja2s90*n$9_*1x2vri_(jtqxshl7lt3bd1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = []
 
 
