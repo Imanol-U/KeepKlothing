a9e17d44d320c50ac97fba8d1e298adf966e4949
bluedragon011 <adrian.baz@opendeusto.es>
2025-11-11 18:57:27 +0100

    Registradas las clases en el admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Categoria, Producto, Compra, ProductoCompra, Usuario
+
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(Usuario)
+admin.site.register(Compra)
+admin.site.register(ProductoCompra)
