0ee5914868bb8c03343819364937d6b631f1e0d2
Imanol-U <imanol.u@opendeusto.es>
2025-11-13 12:43:34 +0100

    Views TopVentas

--- app/views.py
+++ app/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.db.models import Sum
 from django.http import HttpResponse
-from .models import Producto
+from .models import Producto, ProductoCompra
 
 # Create your views here.
 
 def home(request):
     
-    return render(request, "home.html", {})
+    productos_vendidos = ProductoCompra.objects.values('producto__id_producto', 'producto__nombre', 'producto__precio') \
+            .annotate(total_vendido=Sum('cantidad')) \
+            .order_by('-total_vendido')
+            
+    top_n = productos_vendidos[:2]
+
+    context = {
+            'top_productos': top_n
+        }
+
+    return render(request, "home.html", context)
