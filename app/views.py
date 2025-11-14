25d190d2f2654b6f8bd804a803d88162987d0c7a
Imanol-U <imanol.u@opendeusto.es>
2025-11-14 10:55:21 +0100

    Header Update + Home View

--- app/views.py
+++ app/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render
 from django.db.models import Sum
+from django.utils import timezone
+from datetime import timedelta
 from django.http import HttpResponse
 from .models import Producto, ProductoCompra, Categoria
 
 # Create your views here.
 
 def home(request):
-    
-    productos_vendidos = ProductoCompra.objects.values('producto__id_producto', 'producto__nombre', 'producto__precio') \
-            .annotate(total_vendido=Sum('cantidad')) \
-            .order_by('-total_vendido')
-            
-    top_n = productos_vendidos[:2]
 
+    fecha_hace_30_dias = timezone.now() - timedelta(days=30)
+
+    top_productos_tendencia = ProductoCompra.objects.filter(
+            compra__fecha__gte=fecha_hace_30_dias
+        ).values(
+            'producto__id_producto',
+            'producto__nombre',
+            'producto__imagen_url',
+            'producto__precio' #
+        ).annotate(
+            total_vendido=Sum('cantidad')
+        ).order_by('-total_vendido')[:3]
+    
+    productos_mas_vendidos_total = ProductoCompra.objects.values(
+            'producto__id_producto',
+            'producto__nombre',
+            'producto__imagen_url'
+        ).annotate(
+            total_vendido=Sum('cantidad')
+        ).order_by('-total_vendido')[:3]
+    
     context = {
-            'top_productos': top_n
+            'productos_tendencia': top_productos_tendencia,
+            'mas_vendidos' : productos_mas_vendidos_total,
         }
 
     return render(request, "home.html", context)
